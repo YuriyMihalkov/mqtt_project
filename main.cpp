add9bf1590639d844baf83e9d9f354a7fa522628
Yuriy Mihalkov <yuriy.mihalkov@softccom.su>
2025-03-11 21:47:34 +0300

    add file to project

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
+// Copyright (C) 2017 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-int main() {   
-    return 0;
-}   
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
